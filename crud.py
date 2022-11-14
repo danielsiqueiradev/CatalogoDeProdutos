359728d5982362389058fe4486b031fef918dc7f
Daniel Siqueira <75674258+danielsiqueiradev@users.noreply.github.com>
2022-11-13 22:04:28 -0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,139 +1,217 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 12 20:47:20 2020
+Created on Sat Sep 12 19:43:22 2020
 
 @author: smonteiro
 """
-# -----------------------------------------------------------------------------
-# Essa classe possui métodos CRUD
-# -----------------------------------------------------------------------------
-import psycopg2
+
+import tkinter as tk
+from tkinter import ttk
+import crud as crud
 
 
-class AppBD:
-    def __init__(self):
-        print('Método Construtor')
+class PrincipalBD:
+    def __init__(self, win):
+        self.objBD = crud.AppBD()
+        # componentes
+        self.lbCodigo = tk.Label(win, text='Código do Produto:')
+        self.lblNome = tk.Label(win, text='Nome do Produto')
+        self.lblPreco = tk.Label(win, text='Preço')
+        self.lblPrecoVenda = tk.Label(win, text='Preço de Venda')
 
-    def abrirConexao(self):
+        self.txtCodigo = tk.Entry(bd=3)
+        self.txtNome = tk.Entry()
+        self.txtPreco = tk.Entry()
+        self.txtPrecoVenda = tk.Entry()
+        self.btnCadastrar = tk.Button(win, text='Cadastrar', command=self.fCadastrarProduto)
+        self.btnAtualizar = tk.Button(win, text='Atualizar', command=self.fAtualizarProduto)
+        self.btnExcluir = tk.Button(win, text='Excluir', command=self.fExcluirProduto)
+        self.btnLimpar = tk.Button(win, text='Limpar', command=self.fLimparTela)
+        # ----- Componente TreeView --------------------------------------------
+        self.dadosColunas = ("Código", "Nome", "Preço", "Preço de Venda")
+
+        self.treeProdutos = ttk.Treeview(win,
+                                         columns=self.dadosColunas,
+                                         selectmode='browse')
+
+        self.verscrlbar = ttk.Scrollbar(win,
+                                        orient="vertical",
+                                        command=self.treeProdutos.yview)
+        self.verscrlbar.pack(side='left', fill='x')
+
+        self.treeProdutos.configure(yscrollcommand=self.verscrlbar.set)
+
+        self.treeProdutos.heading("Código", text="Código")
+        self.treeProdutos.heading("Nome", text="Nome")
+        self.treeProdutos.heading("Preço", text="Preço")
+        self.treeProdutos.heading("Preço de Venda", text="Preço de Venda")
+
+        self.treeProdutos.column("Código", minwidth=0, width=100)
+        self.treeProdutos.column("Nome", minwidth=0, width=100)
+        self.treeProdutos.column("Preço", minwidth=0, width=100)
+        self.treeProdutos.column("Preço de Venda", minwidth=0, width=100)
+
+        self.treeProdutos.pack(padx=10, pady=10)
+
+        self.treeProdutos.bind("<<TreeviewSelect>>",
+                               self.apresentarRegistrosSelecionados)
+        # ---------------------------------------------------------------------
+        # posicionamento dos componentes na janela
+        # ---------------------------------------------------------------------
+        self.lbCodigo.place(x=100, y=50)
+        self.txtCodigo.place(x=250, y=50)
+
+        self.lblNome.place(x=100, y=100)
+        self.txtNome.place(x=250, y=100)
+
+        self.lblPreco.place(x=100, y=150)
+        self.txtPreco.place(x=250, y=150)
+
+        self.btnCadastrar.place(x=100, y=200)
+        self.btnAtualizar.place(x=200, y=200)
+        self.btnExcluir.place(x=300, y=200)
+        self.btnLimpar.place(x=400, y=200)
+
+        self.treeProdutos.place(x=100, y=300)
+        self.verscrlbar.place(x=700, y=300, height=225)
+        self.carregarDadosIniciais()
+
+
+    # -----------------------------------------------------------------------------
+    def apresentarRegistrosSelecionados(self, event):
+        self.fLimparTela()
+        for selection in self.treeProdutos.selection():
+            item = self.treeProdutos.item(selection)
+            codigo, nome, preco = item["values"][0:3]
+            self.txtCodigo.insert(0, codigo)
+            self.txtNome.insert(0, nome)
+            self.txtPreco.insert(0, preco)
+            self.txtPrecoVenda.insert(0, preco)
+
+    # -----------------------------------------------------------------------------
+
+    def carregarDadosIniciais(self):
         try:
-            self.connection = psycopg2.connect(user="postgres",
-                                               password="1234",
-                                               host="127.0.0.1",
-                                               port="5432",
-                                               database="postgres")
-        except (Exception, psycopg2.Error) as error:
-            if (self.connection):
-                print("Falha ao se conectar ao Banco de Dados", error)
+            self.id = 0
+            self.iid = 0
+            registros = self.objBD.selecionarDados
+            print("************ dados dsponíveis no BD ***********")
+            for item in registros:
+                codigo = item[0]
+                nome = item[1]
+                preco = item[2]
+                precovenda = item[3]
+                print("Códig0 = ", codigo)
+                print("Nome = ", nome)
+                print("Preço  = ", preco)
+                print("Preço de Venda  = ", precovenda, "\n")
 
+                self.treeProdutos.insert('', 'end',
+                                         iid=self.iid,
+                                         values=(codigo,
+                                                 nome,
+                                                 preco,
+                                                 precovenda))
+
+                self.iid = self.iid + 1
+                self.id = self.id + 1
+            print('Dados da Base')
+        except:
+            print('Ainda não existem dados para carregar')
+        # -----------------------------------------------------------------------------
+
+    # LerDados da Tela
     # -----------------------------------------------------------------------------
-    # Selecionar todos os Produtos
-    # -----------------------------------------------------------------------------
-    @property
-    def selecionarDados(self):
+    def fLerCampos(self):
         try:
-            self.abrirConexao()
-            cursor = self.connection.cursor()
-
-            print("Selecionando todos os produtos")
-            sql_select_query = """select * from public.PRODUTO """
-
-            cursor.execute(sql_select_query)
-            registros = cursor.fetchall()
-            print(registros)
-
-
-        except (Exception, psycopg2.Error) as error:
-            print("Error in select operation", error)
-
-        finally:
-            # closing database connection.
-            if (self.connection):
-                cursor.close()
-                self.connection.close()
-                print("A conexão com o PostgreSQL foi fechada.")
-        return registros
+            print("************ dados dsponíveis ***********")
+            codigo = int(self.txtCodigo.get())
+            print('codigo', codigo)
+            nome = self.txtNome.get()
+            print('nome', nome)
+            preco = float(self.txtPreco.get())
+            print('preco', preco)
+            # precovenda = float(self.txtPrecoVenda.get())
+            # print('preco', precovenda)
+            calcprecovenda = preco+preco*0.1
+            print('Leitura dos Dados com Sucesso!')
+        except:
+            print('Não foi possível ler os dados.')
+        return codigo, nome, preco, calcprecovenda
 
     # -----------------------------------------------------------------------------
-    # Inserir Produto
+    # Cadastrar Produto
     # -----------------------------------------------------------------------------
-    def inserirDados(self, codigo, nome, preco, precovenda):
+    def fCadastrarProduto(self):
         try:
-            self.abrirConexao()
-            cursor = self.connection.cursor()
-            postgres_insert_query = """ INSERT INTO PRODUTO 
-          (codigo, NOME, PRECO, precovenda) VALUES (%s,%s,%s,%s)"""
-            record_to_insert = (codigo, nome, preco, precovenda)
-            cursor.execute(postgres_insert_query, record_to_insert)
-            self.connection.commit()
-            count = cursor.rowcount
-            print(count, "Registro inserido com successo na tabela PRODUTO")
-        except (Exception, psycopg2.Error) as error:
-            if (self.connection):
-                print("Falha ao inserir registro na tabela PRODUTO", error)
-        finally:
-            # closing database connection.
-            if (self.connection):
-                cursor.close()
-                self.connection.close()
-                print("A conexão com o PostgreSQL foi fechada.")
+            print("************ dados dsponíveis ***********")
+            codigo, nome, preco, precovenda = self.fLerCampos()
+            self.objBD.inserirDados(codigo, nome, preco, precovenda)
+            self.treeProdutos.insert('', 'end',
+                                     iid=self.iid,
+                                     values=(codigo,
+                                             nome,
+                                             preco,
+                                             precovenda))
+            self.iid = self.iid + 1
+            self.id = self.id + 1
+            self.fLimparTela()
+            print('Produto Cadastrado com Sucesso!')
+        except:
+            print('Não foi possível fazer o cadastro.')
 
     # -----------------------------------------------------------------------------
     # Atualizar Produto
     # -----------------------------------------------------------------------------
-    def atualizarDados(self, codigo, nome, preco, precovenda):
+    def fAtualizarProduto(self):
         try:
-            self.abrirConexao()
-            cursor = self.connection.cursor()
-            print("Registro Antes da Atualização ")
-            sql_select_query = """select * from public.PRODUTO 
-            where CODIGO = %s"""
-            cursor.execute(sql_select_query, (codigo,))
-            record = cursor.fetchone()
-            print(record)
-            # Atualizar registro
-            sql_update_query = """Update public.PRODUTO set NOME = %s, 
-            PRECO = %s, precovenda = (preco+(preco*0.1))
-             where CODIGO = %s"""
-            cursor.execute(sql_update_query, (nome, preco, codigo))
-            self.connection.commit()
-            count = cursor.rowcount
-            print(count, "Registro atualizado com sucesso! ")
-            print("Registro Depois da Atualização ")
-            sql_select_query = """select * from public.PRODUTO 
-            where CODIGO = %s"""
-            cursor.execute(sql_select_query, (codigo,))
-            record = cursor.fetchone()
-            print(record)
-        except (Exception, psycopg2.Error) as error:
-            print("Erro na Atualização", error)
-        finally:
-            # closing database connection.
-            if (self.connection):
-                cursor.close()
-                self.connection.close()
-                print("A conexão com o PostgreSQL foi fechada.")
+            print("************ dados dsponíveis ***********")
+            codigo, nome, preco, precovenda = self.fLerCampos()
+            self.objBD.atualizarDados(codigo, nome, preco, precovenda)
+            # recarregar dados na tela
+            self.treeProdutos.delete(*self.treeProdutos.get_children())
+            self.carregarDadosIniciais()
+            self.fLimparTela()
+            print('Produto Atualizado com Sucesso!')
+        except:
+            print('Não foi possível fazer a atualização.')
 
     # -----------------------------------------------------------------------------
     # Excluir Produto
     # -----------------------------------------------------------------------------
-    def excluirDados(self, codigo):
+    def fExcluirProduto(self):
         try:
-            self.abrirConexao()
-            cursor = self.connection.cursor()
-            # Atualizar registro
-            sql_delete_query = """Delete from public.PRODUTO 
-            where CODIGO = %s"""
-            cursor.execute(sql_delete_query, (codigo,))
-            self.connection.commit()
-            count = cursor.rowcount
-            print(count, "Registro excluído com sucesso! ")
-        except (Exception, psycopg2.Error) as error:
-            print("Erro na Exclusão", error)
-        finally:
-            # closing database connection.
-            if (self.connection):
-                cursor.close()
-                self.connection.close()
-                print("A conexão com o PostgreSQL foi fechada.")
+            print("************ dados dsponíveis ***********")
+            codigo, nome, preco, precovenda = self.fLerCampos()
+            self.objBD.excluirDados(codigo)
+            # recarregar dados na tela
+            self.treeProdutos.delete(*self.treeProdutos.get_children())
+            self.carregarDadosIniciais()
+            self.fLimparTela()
+            print('Produto Excluído com Sucesso!')
+        except:
+            print('Não foi possível fazer a exclusão do produto.')
 
+    # -----------------------------------------------------------------------------
+    # Limpar Tela
+    # -----------------------------------------------------------------------------
+    def fLimparTela(self):
+        try:
+            print("************ dados dsponíveis ***********")
+            self.txtCodigo.delete(0, tk.END)
+            self.txtNome.delete(0, tk.END)
+            self.txtPreco.delete(0, tk.END)
+            print('Campos Limpos!')
+        except:
+            print('Não foi possível limpar os campos.')
+
+
+# -----------------------------------------------------------------------------
+# Programa Principal
+# -----------------------------------------------------------------------------
+janela = tk.Tk()
+principal = PrincipalBD(janela)
+janela.title('Suffered Baby')
+janela.geometry("820x600+10+10")
+janela.mainloop()
 # -----------------------------------------------------------------------------
